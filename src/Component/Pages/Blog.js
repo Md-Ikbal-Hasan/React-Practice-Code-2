12945daf78fc3a288f9c26b33ba030feab5c02af
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-08-11 11:39:32 +0600

    useLocation added

--- src/Component/Pages/Blog.js
+++ src/Component/Pages/Blog.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import { blogsData } from '../../data';
+
 const Blog = () => {
     const { title } = useParams();
-    const [bodyData, setBodyData] = useState("");
-    useEffect(() => {
-        const blogData = blogsData.filter(blog => blog.title === title);
-        setBodyData(blogsData[0].body)
+    const location = useLocation();
+    // console.log("Location: ", location);
 
+    // const [bodyData, setBodyData] = useState("");
+    // useEffect(() => {
+    //     const blogData = blogsData.filter(blog => blog.title === title);
+    //     setBodyData(blogsData[0].body)
 
-    }, [])
+    // }, [])
 
     const navigate = useNavigate();
 
     return (
         <div>
             <h1>{title} Page </h1>
-            <p> {bodyData.slice(0, 500)} </p>
-            <p> {bodyData.slice(501, 5000)} </p>
+            <p> {location.state.body.slice(0, 500)} </p>
+            <p> {location.state.body.slice(501, 5000)} </p>
 
             <button onClick={() => {
                 navigate("/blogs")
