04be406c7e466e9d1ddb9679fceece0a814ed18b
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-08-11 08:04:03 +0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Component/Pages/Home';
+import Contact from './Component/Pages/Contact';
+import Blogs from './Component/Pages/Blogs';
+import Error from './Component/Pages/Error';
 
 function App() {
   return (
-    <div >
-      hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
 
 
   );
---
+++ src/Component/Pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact page</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae nemo, cum consectetur architecto unde nisi commodi temporibus. Doloremque quo fugit nostrum. Nesciunt dicta ducimus sequi doloribus natus, blanditiis similique ea assumenda aspernatur accusamus laudantium nemo nostrum dolor dolorem officia nulla doloremque ipsum asperiores! Labore consequatur dignissimos accusantium facilis illo esse!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore atque ipsam ad rerum at, itaque possimus accusamus asperiores suscipit deserunt perspiciatis voluptatibus et officiis, eos nulla placeat! Inventore, veniam exercitationem.</p>
+        </div>
+    )
+}
+
+export default Contact
